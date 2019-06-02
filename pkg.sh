56503f3f9e496541235466226f48bb21ab66ade5
Aadesh9985 <guravaadesh99@gmail.com>
2019-06-02 21:27:58 +0530

    Add files via upload

---
+++ pkg.sh
@@ -0,0 +1,232 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+sleep 3
+echo "                                                                        "
+echo "                                                                        "
+echo "                                                                        "
+echo "----------------------------Created By Neuron---------------------------"
+echo "----------------------------------For Fun-------------------------------"
+echo "                                                                        "
+echo "                                                                        "
+echo "                                                                        "
+echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+sleep 3
+
+echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "::////////////////////////////////////////////////////////////////////::"
+echo ":://  NeNeNeNeNeNeNeNeNe  UrUr       UrUr  OnOnOnOn            OnOn //::"
+echo ":://  NeNeNeNeNeneNeNeNe  UrUr       UrUr  OnOn OnOn           OnOn //::"
+echo ":://  NeNe                UrUr       UrUr  OnOn  OnOn          OnOn //::"
+echo ":://  Nene                UrUr       UrUr  OnOn   OnOn         OnOn //::"
+echo ":://  Nene                UrUr       UrUr  OnOn    OnOn        OnOn //::"
+echo ":://  NeneNeNeNe          UrUr       UrUr  OnOn     OnOn       OnOn //::"
+echo ":://  NeNeNeNeNe          UrUr       UrUr  OnOn      OnOn      OnOn //::"
+echo ":://  NeNe                UrUr       UrUr  OnOn       OnOn     OnOn //::"
+echo ":://  NeNe                UrUr       UrUr  OnOn        OnOn    OnOn //::"
+echo ":://  NeNe                UrUr       UrUr  OnOn         OnOn   OnOn //::"
+echo ":://  NeNe                 UrUr      UrUr  OnOn          OnOn  OnOn //::"
+echo ":://  NeNe                  UrUrU  rUrUr   OnOn           OnOn OnOn //::"
+echo ":://  NeNe                   UrUrUrUrUr    OnOn            OnOnOnOn //::"
+echo "::////////////////////////////////////////////////////////////////////::"
+echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+
+
+
+
+
+pkg update 
+
+sleep 3
+
+echo "pkg updated"
+
+sleep 3
+
+pkg upgrade 
+
+sleep 3
+
+echo "pkg upgraded"
+
+sleep 3
+
+pkg install git -y
+
+sleep 3
+
+echo "Git Installed"
+
+sleep 3
+ 
+pkg install fish -y
+
+sleep 3
+
+echo "Fish(Friendly Interactive Shell) Installed"
+
+sleep 3
+ 
+pkg install wget -y
+
+sleep 3
+
+echo "wget Installed"
+
+sleep 3
+  
+pkg install curl -y
+
+sleep 3
+
+echo "curl Installed"
+   
+pkg install ruby -y
+
+sleep 3
+
+echo "ruby Installed"
+
+sleep 3
+    
+pkg install perl -y
+
+sleep 3
+
+echo "perl Installed"
+
+sleep 3
+     
+pkg install python -y
+
+sleep 3
+
+echo "python, python3 Installed"
+
+sleep 3
+      
+pkg install python2 -y
+
+sleep 1a33
+
+echo "python2 Installed"
+
+sleep 3
+       
+pkg install nano -y
+
+sleep 3
+
+echo "nano Installed"
+
+sleep 3
+        
+pkg install w3m -y
+
+sleep 3
+
+echo "w3m installed"
+
+sleep 3
+         
+pkg install termux-api -y
+
+sleep 3
+
+echo "termux-api Installed"
+
+sleep 3
+         
+pkg install ccrypt -y
+
+sleep 3
+
+echo "ccrypt Installed"
+
+sleep 3
+         
+pkg install clang -y
+
+sleep 3
+
+echo "clang Installed"
+
+sleep 3
+
+pkg install openssh -y
+
+sleep 3
+
+echo "openssh Installed"
+
+sleep 3
+   
+pkg install php -y
+
+sleep 3
+
+echo "php Installed"
+
+sleep 3
+
+pip install --upgrade pip
+
+echo "pip upgraded"
+
+sleep 3
+
+pip2 install --upgrade pip
+
+echo "pip2 upgraded"
+
+sleep 3
+
+pip install colorama
+
+echo "colorama installed"
+
+sleep 3
+
+pip install beautifulsoup
+
+echo "beautifulsoup installed"
+
+sleep 3
+      
+pip install wget==3.2
+
+sleep 3
+
+echo "wget 3.2 Installed"
+
+sleep 3
+
+pip install bs4
+
+sleep 3
+
+echo "Beautiful Soup 4 Installed"
+
+sleep 3
+
+pip install requests
+
+sleep 5
+
+
+echo "Done"
+
+sleep 3
+
+echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+sleep 3
+echo "                                                                        "
+echo "                                                                        "
+echo "                                                                        "
+echo "-------------------------Succeessfully Installed------------------------"
+echo "-------------------------------Enjoy Termux-----------------------------"
+echo "                                                                        "
+echo "                                                                        "
+echo "                                                                        "
+echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+     
+     
