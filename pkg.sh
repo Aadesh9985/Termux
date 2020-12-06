753d198f961da8e2e8a0d7162a5911fa36bb7f78
Aadesh9985 <guravaadesh99@gmail.com>
2020-12-06 14:50:27 +0530

    Update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
 sleep 3
 echo "                                                                        "
 echo "                                                                        "
@@ -9,7 +9,7 @@ echo "----------------------------------For Fun-------------------------------"
 echo "                                                                        "
 echo "                                                                        "
 echo "                                                                        "
-echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
 sleep 3
 
 echo "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
@@ -38,183 +38,341 @@ pkg update -y
 
 sleep 3
 
+clear
+
 echo "pkg updated"
 
 sleep 3
+
+clear
+
+sleep 1
 
 pkg upgrade -y
 
 sleep 3
 
+clear
+
 echo "pkg upgraded"
 
 sleep 3
+
+clear
+
+sleep 1
 
 pkg install git -y
 
 sleep 3
 
+clear
+
 echo "Git Installed"
 
 sleep 3
+
+clear
+
+sleep 1
  
 pkg install fish -y
 
 sleep 3
 
+clear
+
 echo "Fish(Friendly Interactive Shell) Installed"
 
 sleep 3
+
+clear
+
+sleep 1
  
 pkg install wget -y
 
 sleep 3
 
+clear
+
 echo "wget Installed"
 
 sleep 3
+
+clear
+
+sleep 1
   
 pkg install curl -y
 
 sleep 3
 
+clear
+
 echo "curl Installed"
 
 sleep 3
+
+clear
+
+sleep 1
    
 pkg install ruby -y
 
 sleep 3
 
+clear
+
 echo "ruby Installed"
 
 sleep 3
+
+clear
+
+sleep 1
     
 pkg install perl -y
 
 sleep 3
 
+clear
+
 echo "perl Installed"
 
 sleep 3
+
+clear
+
+sleep 1
      
 pkg install python -y
 
 sleep 3
 
+clear
+
 echo "python, python3 Installed"
 
 sleep 3
+
+clear
+
+sleep 1
       
 pkg install python2 -y
 
 sleep 3
 
+clear
+
 echo "python2 Installed"
 
 sleep 3
+
+clear
+
+sleep 1
        
 pkg install nano -y
 
 sleep 3
 
+clear
+
 echo "nano Installed"
 
 sleep 3
+
+clear
+
+sleep 1
         
 pkg install w3m -y
 
 sleep 3
 
+clear
+
 echo "w3m installed"
 
 sleep 3
+
+clear
+
+sleep 1
          
 pkg install termux-api -y
 
 sleep 3
 
+clear
+
 echo "termux-api Installed"
 
 sleep 3
+
+clear
+
+sleep 1
          
 pkg install ccrypt -y
 
 sleep 3
 
+clear
+
 echo "ccrypt Installed"
 
 sleep 3
+
+clear
+
+sleep 1
          
 pkg install clang -y
 
 sleep 3
 
+clear
+
 echo "clang Installed"
 
 sleep 3
+
+clear
+
+sleep 1
 
 pkg install openssh -y
 
 sleep 3
 
+clear
+
 echo "openssh Installed"
 
 sleep 3
+
+clear
+
+sleep 1
    
 pkg install php -y
 
 sleep 3
 
+clear
+
 echo "php Installed"
 
 sleep 3
 
+clear
+
+sleep 1
+
 pip install --upgrade pip
+
+sleep 3
+
+clear
 
 echo "pip upgraded"
 
 sleep 3
 
+clear
+
+sleep 1
+
 pip2 install --upgrade pip
+
+sleep 3
+
+clear
 
 echo "pip2 upgraded"
 
 sleep 3
 
+clear
+
+sleep 1
+
 pip install colorama
+
+sleep 3
+
+clear
 
 echo "colorama installed"
 
 sleep 3
 
+clear
+
+sleep 1
+
 pip install beautifulsoup
+
+sleep 3
+
+clear
 
 echo "beautifulsoup installed"
 
 sleep 3
+
+clear
+
+sleep 1
       
 pip install wget==3.2
 
 sleep 3
 
+clear
+
 echo "wget 3.2 Installed"
 
 sleep 3
+
+clear
+
+sleep 1
 
 pip install bs4
 
 sleep 3
 
+clear
+
 echo "Beautiful Soup 4 Installed"
 
 sleep 3
+
+clear
+
+sleep 1
 
 pip install requests
 
 sleep 3
 
+clear
+
+echo "Requests Installed"
+
+sleep 3
+
+clear
+
+sleep 1
+
 apt full-upgrade -y
+
+clear
 
 sleep 5
 
@@ -223,8 +381,7 @@ echo "Done"
 
 sleep 3
 
-echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-sleep 3
+echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
 echo "                                                                        "
 echo "                                                                        "
 echo "                                                                        "
@@ -233,6 +390,6 @@ echo "-------------------------------Enjoy Termux-----------------------------"
 echo "                                                                        "
 echo "                                                                        "
 echo "                                                                        "
-echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
      
      
